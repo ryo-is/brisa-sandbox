7d6fff25663a934983890e0b413f6f22f94e8031
ryo-is <ryosuke.izumi62@gmail.com>
2024-10-10 08:55:22 +0900

    style: add biome

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -3,9 +3,9 @@ import { describe, expect, it } from 'bun:test';
 import Home from '.';
 
 describe('Index', () => {
-  it('should render "Welcome to Brisa"', async () => {
-    const { container } = await render(<Home />);
+	it('should render "Welcome to Brisa"', async () => {
+		const { container } = await render(<Home />);
 
-    expect(container).toContainTextContent('Welcome to Brisa');
-  });
+		expect(container).toContainTextContent('Welcome to Brisa');
+	});
 });
